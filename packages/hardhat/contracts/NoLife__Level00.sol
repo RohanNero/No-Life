c1fca4371f617ab006fa46e7f05bff4448736687
Rohan <RohanZarathustra@gmail.com>
2023-04-01 23:19:52 -0400

    buidling

--- packages/hardhat/contracts/NoLife__Level00.sol
+++ packages/hardhat/contracts/NoLife__Level00.sol
@@ -2,26 +2,24 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 // Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
 // Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+error NoLife__Level00__IncorrectEncoding();
+
 /**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
+ * @title No-Life Level 00
+ * @author Rohan Nero
+ * @notice this is the first level in the No-Life game
+ * @dev encoding functions prefaced with "encode" while decoding ones are prefaced with "decode"
  */
-contract YourContract {
+   contract NoLife__Level00  {
 
-    // State Variables
-    address public immutable owner;
-    string public greeting = "Building Unstoppable Apps!!!";
-    bool public premium = false;
-    uint256 public totalCounter = 0;
-    mapping(address => uint) public userGreetingCounter;
-
-    // Events: a way to emit log statements from smart contract that can be listened to by external parties
-    event GreetingChange(address greetingSetter, string newGreeting, bool premium, uint256 value);
+    address public owner;
+    // This variable is used to track which function you are encoding/decoding 
+    // The frontend button will pass this as the argument to the `submitAnswer` function
+    uint public counter; 
 
     // Constructor: Called once on contract deployment
     // Check packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -31,48 +29,37 @@ contract YourContract {
 
     // Modifier: used to define a set of rules that must be met before or after a function is executed
     // Check the withdraw() function
-    modifier isOwner() {
+    modifier onlyOwner() {
         // msg.sender: predefined variable that represents address of the account that called the current function
         require(msg.sender == owner, "Not the Owner");
         _;
     }
+    function decode__doctorMorris(bytes32 answer) public onlyOwner {
+        if(answer == this.decode__doctorMorris.selector) {
+            counter++;
+        } //else {
+            //revert NoLife__Level00__IncorrectInput();
+        //}
+    }
+    
 
-    /**
-     * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-     *
-     * @param _newGreeting (string memory) - new greeting to save on the contract
-     */
-    function setGreeting(string memory _newGreeting) public payable {
-        // Print data to the hardhat chain console. Remove when deploying to a live network.
-        console.log("Setting new greeting '%s' from %s",  _newGreeting, msg.sender);
 
-        // Change state variables
-        greeting = _newGreeting;
-        totalCounter += 1;
-        userGreetingCounter[msg.sender] += 1;
+    /**@notice this function is called when players fire their `CODING RAY` while set to encode */
+    function encode(bytes memory answer, uint count) public onlyOwner {
+        if(count == 0 /*&& answer == this.decode__doctorMorris */) {
+            counter++;
+        } 
 
-        // msg.value: built-in global variable that represents the amount of ether sent with the transaction
-        if (msg.value > 0) {
-            premium = true;
-        } else {
-            premium = false;
+
+
+
+        revert NoLife__Level00__IncorrectEncoding();
+    }
+    
+    /**@notice this function is called when players fire their `CODING RAY` while set to decode */
+    function decode(bytes memory answer, uint count) public onlyOwner {
+        if(count == 0/* && answer*/ ) {
+
         }
-
-        // emit: keyword used to trigger an event
-        emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, 0);
     }
-
-    /**
-     * Function that allows the owner to withdraw all the Ether in the contract
-     * The function can only be called by the owner of the contract as defined by the isOwner modifier
-     */
-    function withdraw() isOwner public {
-        (bool success,) = owner.call{value: address(this).balance}("");
-        require(success, "Failed to send Ether");
-    }
-
-    /**
-     * Function that allows the contract to receive ETH
-     */
-    receive() external payable {}
 }
