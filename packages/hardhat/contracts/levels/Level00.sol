89d5a16a4456952cff7ec85adf0770ccff0d7e9e
Rohan <RohanZarathustra@gmail.com>
2023-04-02 23:39:05 -0400

    working on logic and structure

--- packages/hardhat/contracts/levels/Level00.sol
+++ packages/hardhat/contracts/levels/Level00.sol
@@ -1,64 +1,70 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+error Level00__AlreadyPassed(uint count, uint problemId);
+
+contract Level00 {
+
+    string _name;
+    uint public x;
+    uint public count;
 
 
-error NoLife__Level00__IncorrectEncoding();
+    /**@notice The Problems */
+    bytes public DrMorris;
+    bytes public DrTom;
+    
 
-/**
- * @title No-Life Level 00
- * @author Rohan Nero
- * @notice this is the first level in the No-Life game
- * @dev encoding functions prefaced with "encode" while decoding ones are prefaced with "decode"
- */
-   contract NoLife__Level00  {
+    /**@notice emitted when provided incorrect calldata */
+    event Level00__ZeroDamage();
 
-    address public owner;
-    // This variable is used to track which function you are encoding/decoding 
-    // The frontend button will pass this as the argument to the `submitAnswer` function
-    uint public counter; 
-
-    // Constructor: Called once on contract deployment
-    // Check packages/hardhat/deploy/00_deploy_your_contract.ts
-    constructor(address _owner) {
-        owner = _owner;
-    }
-
-    // Modifier: used to define a set of rules that must be met before or after a function is executed
-    // Check the withdraw() function
-    modifier onlyOwner() {
-        // msg.sender: predefined variable that represents address of the account that called the current function
-        require(msg.sender == owner, "Not the Owner");
+    modifier onlyFallback() {
+        require(msg.sender != address(this));
         _;
     }
-    function decode__doctorMorris(bytes32 answer) public onlyOwner {
-        if(answer == this.decode__doctorMorris.selector) {
-            counter++;
-        } //else {
-            //revert NoLife__Level00__IncorrectInput();
-        //}
+
+    /**@notice values need to be passed to the contract for */
+    constructor(bytes memory drMorris, bytes memory drTom ) {
+        DrMorris = drMorris;
+        DrTom = drTom;
     }
-t
 
-    /**@notice this function is called when players fire their `CODING RAY` while set to encode */
-    function encode(bytes memory answer, uint count) public onlyOwner {
-        if(count == 0 /*&& answer == this.decode__doctorMorris */) {
-            counter++;
-        } 
+    /**@notice this function handles calls to the contract that don't have function selectors
+     * @dev such as `abi.encode(Packed)` values */
+    fallback() external {
+        if(keccak256(msg.data) == keccak256(DrMorris)) {
+            x = 7;
+        } else if(keccak256(msg.data) == keccak256(DrTom)) {
 
-
-
-
-        revert NoLife__Level00__IncorrectEncoding();
-    }
-    
-    /**@notice this function is called when players fire their `CODING RAY` while set to decode */
-    function decode(bytes memory answer, uint count) public onlyOwner {
-        if(count == 0/* && answer*/ ) {
-
+        } else {
+            emit Level00__ZeroDamage();
         }
+    }
+
+
+
+    /** ------------------------------------------ 
+                    Main Functions
+        ------------------------------------------ 
+        * Functions are numbered for organization and once a function is called with the correct input,
+        * it cannot be called again. 
+        * Wherever you see `count++`, that means the level is solved!
+        */
+
+
+
+    /**@notice the first person to save! (problem to solve)
+     * @dev the fallback function calls this */
+    function DoctorMorris() external onlyFallback{
+        uint problemId = 0;
+        if(count > 0) {
+            revert Level00__AlreadyPassed(count, problemId);
+        }
+        count++;
+    }
+
+    function DoctorTom(string memory name, uint age) external {
+        _name = name;
+        x = age;
     }
 }
---
+++ packages/hardhat/contracts/levels/Level03.sol
@@ -0,0 +1,70 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+error LEVEL03__AlreadyPassed(uint count, uint problemId);
+
+contract Level03 {
+
+    string _name;
+    uint public x;
+    uint public count;
+
+
+    /**@notice The Problems */
+    bytes public DrMorris;
+    bytes public DrTom;
+    
+
+    /**@notice emitted when provided incorrect calldata */
+    event LEVEL03__ZeroDamage();
+
+    modifier onlyFallback() {
+        require(msg.sender != address(this));
+        _;
+    }
+
+    /**@notice values need to be passed to the contract for */
+    constructor(bytes memory drMorris, bytes memory drTom ) {
+        DrMorris = drMorris;
+        DrTom = drTom;
+    }
+
+    /**@notice this function handles calls to the contract that don't have function selectors
+     * @dev such as `abi.encode(Packed)` values */
+    fallback() external {
+        if(keccak256(msg.data) == keccak256(DrMorris)) {
+            x = 7;
+        } else if(keccak256(msg.data) == keccak256(DrTom)) {
+
+        } else {
+            emit LEVEL03__ZeroDamage();
+        }
+    }
+
+
+
+    /** ------------------------------------------ 
+                    Main Functions
+        ------------------------------------------ 
+        * Functions are numbered for organization and once a function is called with the correct input,
+        * it cannot be called again. 
+        * Wherever you see `count++`, that means the level is solved!
+        */
+
+
+
+    /**@notice the first person to save! (problem to solve)
+     * @dev the fallback function calls this */
+    function DoctorMorris() external onlyFallback{
+        uint problemId = 0;
+        if(count > 0) {
+            revert LEVEL03__AlreadyPassed(count, problemId);
+        }
+        count++;
+    }
+
+    function DoctorTom(string memory name, uint age) external {
+        _name = name;
+        x = age;
+    }
+}
