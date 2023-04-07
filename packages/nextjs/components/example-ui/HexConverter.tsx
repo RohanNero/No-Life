5cf5395d40b21bfb5b240124be8a6bb5496073ba
HazardWarning <brett.bates23@gmail.com>
2023-04-07 19:18:11 -0400

    Made Text Italic

--- packages/nextjs/components/example-ui/HexConverter.tsx
+++ packages/nextjs/components/example-ui/HexConverter.tsx
@@ -27,13 +27,13 @@ export const HexConverter = () => {
       <HareIcon className="absolute right-0 bottom-24" />
       <div className="flex-1 flex-col h-full mx-5 sm:mx-8 mx-20">
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Hex Converter</span>
+          <span className="text-4xl sm:text-6xl text-black text-center">Hex Converter</span>
 
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <input
               type="text"
               placeholder="Enter Here For Conversion"
-              className="input font-bai-jamjuree w-full px-5 bg-red-600 bg-[length:100%_100%] border border-primary text-lg sm:text-2xl placeholder-white"
+              className="input italic font-bai-jamjuree w-full px-5 bg-red-600 bg-[length:100%_100%] border border-primary text-lg sm:text-2xl placeholder-white"
               onChange={e => setHexValue(e.target.value)}
               onKeyPress={hexInput}
             />
