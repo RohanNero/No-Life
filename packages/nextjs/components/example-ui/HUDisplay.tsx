ecab410ae96d266854a6c223cb2d2c9a8190efa5
Rohan <RohanZarathustra@gmail.com>
2023-04-07 15:20:19 -0400

    style hud

--- packages/nextjs/components/example-ui/HUDisplay.tsx
+++ packages/nextjs/components/example-ui/HUDisplay.tsx
@@ -12,11 +12,10 @@ type HUDProps = {
 export const HUDisplay = (props: HUDProps) => {
   const { setDisplayConverter, setCodingRay, setDisplayDecoder } = props;
   return (
-    <div className="flex bg-base-300 relative pb-10">
-      <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
+    <div className="flex bg-base-300 border-top border-red-700 absolute w-1/2 bottom-0 ">
+      <div className="flex flex-col mx-5 w-full sm:mx-8 2xl:mx-20">
+        <span className="text-4xl sm:text-6xl text-black">HUD</span>
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">HUD</span>
-
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
               <div className="flex rounded-full border-2 border-primary p-1">
